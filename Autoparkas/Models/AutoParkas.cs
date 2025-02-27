1b6f2fbf64bfe53fd2f90887d7b384954b417c1d
Sarunas <sarugen12@gmail.com>
2025-02-27 09:30:19 +0200

    Add project files.

---
+++ Autoparkas/Models/AutoParkas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoparkas.Models
+{
+    public class AutoParkas
+    {
+        private Automobilis[] Automobiliai;
+
+        public AutoParkas()
+        {
+            Automobiliai = new Automobilis[0];
+        }
+        public void PridetiAutomobili(Automobilis automobilis)
+        {
+            Automobilis[] naujasMasyvas = new Automobilis[Automobiliai.Length + 1];
+            int index = 0;
+            foreach(Automobilis a in Automobiliai)
+            {
+                naujasMasyvas[index] = a;
+                index++;
+            }
+            naujasMasyvas[index] = automobilis;
+            Automobiliai = naujasMasyvas;
+        }
+        public Automobilis[] GautiVisusAutomobilius()
+        {
+            return Automobiliai;
+        }
+        public Automobilis[] GautiAutomobiliusPagalMarke(string marke)
+        {
+            Automobilis[] automobiliai;
+
+            int kiekis = 0;
+            foreach(Automobilis a in Automobiliai)
+            {
+                if (a.Marke == marke)
+                    kiekis++;
+            }
+            automobiliai = new Automobilis[kiekis];
+            int index = 0;
+            foreach (Automobilis a in Automobiliai)
+            {
+                if (a.Marke == marke)
+                {
+                    automobiliai[index] = a;
+                    index++;
+                }
+            }
+
+            return automobiliai;
+        }
+    }
+}
---
+++ Autoparkas/Program.cs
@@ -0,0 +1,70 @@
+﻿using Autoparkas.Models;
+using System;
+
+namespace Autoparkas
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            AutoParkas manoAutoparkas = new AutoParkas();
+            
+            while(true)
+            {
+                Console.WriteLine("1. Prideti Automobili");
+                Console.WriteLine("2. Rodyti Visus Automobilius");
+                Console.WriteLine("3. Rasti Automobili Pagal Marke");
+                Console.WriteLine("0. Baigti Darba");
+                if(int.TryParse(Console.ReadLine(), out int pasirinkimas))
+                {
+                    switch(pasirinkimas)
+                    {
+                        case 1:
+                            Console.WriteLine("Iveskite marke:");
+                            string marke = Console.ReadLine();
+
+                            Console.WriteLine("Iveskite modeli:");
+                            string modelis = Console.ReadLine();
+
+                            Console.WriteLine("Iveskite pirmosios registracijos data:");
+                            DateOnly pirmRegData = DateOnly.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Iveskite paros kaina:");
+                            decimal parosKaina = decimal.Parse(Console.ReadLine());
+
+                            Automobilis naujasAuto = new Automobilis
+                            {
+                                Marke = marke,
+                                Modelis = modelis,
+                                PirmosRegData = pirmRegData,
+                                ParosKaina = parosKaina
+                            };
+
+                            manoAutoparkas.PridetiAutomobili(naujasAuto);
+                            break;
+                        case 2:
+                            Automobilis[] visiAuto = manoAutoparkas.GautiVisusAutomobilius();
+                            foreach(Automobilis a in visiAuto)
+                            {
+                                Console.WriteLine($"{a.Marke} {a.Modelis} {a.PirmosRegData} {a.ParosKaina}");
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("Iveskite marke:");
+                            string ieskomaMarke = Console.ReadLine();
+
+                            Automobilis[] rastiAuto = manoAutoparkas.GautiAutomobiliusPagalMarke(ieskomaMarke);
+                            foreach (Automobilis a in rastiAuto)
+                            {
+                                Console.WriteLine($"{a.Marke} {a.Modelis} {a.PirmosRegData} {a.ParosKaina}");
+                            }
+                            break;
+                        case 0:
+                            return;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
